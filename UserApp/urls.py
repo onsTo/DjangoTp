32aa5353d40633be1c69cbc657eaa2ee340c343f
onstouati <152779749+onsTo@users.noreply.github.com>
2025-11-06 11:08:23 +0100

    Partie Client (Views)(login/logout/registrer)

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
