b6e4a6539569825ff4a2411f47e6707256ab07e2
onstouati <152779749+onsTo@users.noreply.github.com>
2025-11-10 15:31:11 +0100

    Submission

--- ConferenceApp/urls.py
+++ ConferenceApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 #from .views import *
 from .views import ConfernceList, ConfernceDetail, ConferenceCreate, ConferenceUpdate, ConferenceDelete
+from .views import ListSubmissions, SubmissionDetailView, AddSubmissionView, UpdateSubmissionView
+
 
 urlpatterns = [
     #path("liste/", views.all_conferences, name="Conferences_list"), #importation de fonction
@@ -10,5 +12,13 @@ urlpatterns = [
     path("form/", ConferenceCreate.as_view(), name="Conference_add"),
     path("<int:pk>/edit/", ConferenceUpdate.as_view(), name="Conference_edit"),
     path("<int:pk>/delete/", ConferenceDelete.as_view(), name="Conference_delete"),
+    #submissions
+    path('conference/<int:conference_id>/submissions/', ListSubmissions.as_view(), name='list_submissions'),
+    path('submission/add/', AddSubmissionView.as_view(), name='add_submission'),
+    path('submission/<str:pk>/', SubmissionDetailView.as_view(), name='submission_detail'),
+    path('submission/<str:pk>/edit/', UpdateSubmissionView.as_view(), name='update_submission'),
+
+
+
     
 ]
--- ConferenceApp/views.py
+++ ConferenceApp/views.py
@@ -3,7 +3,11 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from .models import Conference
 from .forms import ConferenceForm
-from django.contrib.auth.mixins import LoginRequiredMixin # ili connecter khw youdkhel il update delete add
+from django.contrib.auth.mixins import LoginRequiredMixin  # ili connecter khw youdkhel il update delete add
+from .models import Submission
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+
 
 # Create your views here.
 def all_conferences(request):
@@ -47,3 +51,58 @@ class ConferenceDelete(LoginRequiredMixin,DeleteView):
     success_url = reverse_lazy('Conferences_list')
     template_name = 'conference/conference_confirm_delete.html'
 
+class ListSubmissions(ListView):
+    model = Submission
+    template_name = 'submissions/list_submissions.html'  # à créer
+    context_object_name = 'submissions'
+
+    def get_queryset(self):
+        # Récupérer l'ID de la conférence depuis l'URL
+        conference_id = self.kwargs.get('conference_id')
+        conference = get_object_or_404(Conference, pk=conference_id)
+        return Submission.objects.filter(conference=conference).order_by('-submission_date')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['conference'] = get_object_or_404(Conference, pk=self.kwargs.get('conference_id'))
+        return context
+
+class SubmissionDetailView(DetailView):
+    model = Submission
+    template_name = 'submissions/submission_detail.html'  # à créer
+    context_object_name = 'submission'
+    pk_url_kwarg = 'pk'  # correspond à <str:pk> dans l'URL
+
+
+class AddSubmissionView(CreateView):
+    model = Submission
+    template_name = 'submissions/add_submission.html'
+    fields = ['title', 'abstract', 'keywords', 'paper']  # retirer 'conference'
+
+    def form_valid(self, form):
+        # l'utilisateur connecté devient l'auteur
+        form.instance.userid = self.request.user
+
+        # récupérer la conférence depuis la query string
+        conference_id = self.request.GET.get('conference_id')
+        conference = get_object_or_404(Conference, pk=conference_id)
+        form.instance.conference = conference
+
+        return super().form_valid(form)
+    
+
+    def get_success_url(self):
+        return reverse_lazy('list_submissions', kwargs={'conference_id': self.object.conference.pk})
+class UpdateSubmissionView(UpdateView):
+    model = Submission
+    fields = ['title', 'abstract', 'keywords', 'paper', 'status', 'payed']
+    template_name = 'submissions/update_submission.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.userid != request.user:
+            raise PermissionDenied("Vous n'avez pas la permission de modifier cette soumission.")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('list_submissions', kwargs={'conference_id': self.object.conference.pk})
